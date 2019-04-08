78e0f1a83074a91157156cd570f114cb56ab4159
Ethan Atkins <ethan.atkins@gmail.com>
2019-04-08 14:22:12 -0700

    improve filetreeview impl

--- io/src/main/scala/sbt/internal/nio/DefaultFileTreeView.scala
+++ io/src/main/scala/sbt/internal/nio/DefaultFileTreeView.scala
@@ -10,14 +10,16 @@
 
 package sbt.internal.nio
 
-import java.nio.file.{ NoSuchFileException, NotDirectoryException, Path => NioPath }
+import java.nio.file._
+import java.util
 
 import com.swoval.files.{ FileTreeViews, TypedPath }
 import sbt.internal.io.Retry
-import sbt.internal.nio.SwovalConverters._
 import sbt.nio.{ FileAttributes, FileTreeView, Glob }
 
+import scala.annotation.tailrec
 import scala.collection.JavaConverters._
+import scala.collection.immutable.VectorBuilder
 
 private[sbt] object DefaultFileTreeView extends FileTreeView.Nio[FileAttributes] {
   private[this] val fileTreeView =
@@ -26,35 +28,53 @@ private[sbt] object DefaultFileTreeView extends FileTreeView.Nio[FileAttributes]
     else
       FileTreeViews.getDefault(true)
 
-  override def list(glob: Glob): Seq[(NioPath, FileAttributes)] =
+  private def listImpl(glob: Glob): Vector[(Path, FileAttributes)] = {
+    val result = new VectorBuilder[(Path, FileAttributes)]
+    val queue = new util.LinkedList[Path]
+    queue.add(glob.base)
+    def toFileAttributes(typedPath: TypedPath): FileAttributes =
+      FileAttributes(isDirectory = typedPath.isDirectory,
+                     isOther = false,
+                     isRegularFile = typedPath.isFile,
+                     isSymbolicLink = typedPath.isSymbolicLink)
+    def maybeAdd(typedPath: TypedPath): Unit = {
+      val path = typedPath.getPath
+      if (glob.filter(path)) result += path -> toFileAttributes(typedPath)
+    }
+    @tailrec
+    def impl(): Unit = {
+      queue.poll match {
+        case null =>
+        case path =>
+          val depth = if (glob.base == path) 0 else glob.base.relativize(path).getNameCount
+          fileTreeView.list(path, 0, (_: TypedPath) => true).asScala.foreach {
+            case p if p.isDirectory =>
+              if (depth + 1 <= glob.range._2) queue.add(p.getPath)
+              maybeAdd(p)
+            case f => maybeAdd(f)
+          }
+          impl()
+      }
+    }
+    impl()
+    result.result
+  }
+  override def list(glob: Glob): Seq[(Path, FileAttributes)] =
     Retry {
       try {
-        val collector: TypedPath => Seq[(NioPath, FileAttributes)] = typedPath => {
-          val path = typedPath.getPath
-          val attributes = FileAttributes(isDirectory = typedPath.isDirectory,
-                                          isOther = false,
-                                          isRegularFile = typedPath.isFile,
-                                          isSymbolicLink = typedPath.isSymbolicLink)
-          val pair = path -> attributes
-          if (glob.filter(path)) pair :: Nil else Nil
-        }
+        val base = glob.base
         (if (glob.range._1 == 0) {
-           fileTreeView
-             .list(glob.base, -1, (_: TypedPath) => true)
-             .asScala
-             .flatMap(collector)
-             .toIndexedSeq
-         } else Vector.empty[(NioPath, FileAttributes)]) ++ (if (glob.range._2 > 0)
-                                                               fileTreeView
-                                                                 .list(glob.base,
-                                                                       glob.range.toSwovalDepth,
-                                                                       (_: TypedPath) => true)
-                                                                 .asScala
-                                                                 .flatMap(collector)
-                                                                 .toIndexedSeq
-                                                             else
-                                                               Vector
-                                                                 .empty[(NioPath, FileAttributes)])
+           if (glob.filter(base)) FileAttributes(base) match {
+             case Right(a) => Vector(base -> a)
+             case _        => Vector.empty
+           } else Vector.empty
+         } else Vector.empty[(Path, FileAttributes)]) ++ {
+          if (glob.range._2 > 0)
+            listImpl(glob)
+          else
+            Vector
+              .empty[(Path, FileAttributes)]
+        }
       } catch {
         case _: NoSuchFileException | _: NotDirectoryException =>
           Nil
